c33a1763b6436330b7d27663593991ab80f0e157
JaiMoua <mouajai@gmail.com>
2023-10-25 10:19:45 -0500

    pushing react page

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactPlayer from 'react-player'
+import './App.css';
+import './index.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>
+        Project 2
+      </h1>
+      <body>
+        <ReactPlayer url="https://youtu.be/HhzS2j4o5tM"/>
+      </body>
+    </div>
+  );
+}
+
+export default App;
